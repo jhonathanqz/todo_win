ac10dd8f211a105fabea320535e20723182e2772
jhonathanqz <jhonathanqz011@gmail.com>
2022-04-09 14:40:06 -0300

    Create tests from home page

--- test/widget/home/home_page_test.dart
+++ test/widget/home/home_page_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todo_win/app/injection_container.dart';
 import 'package:todo_win/presentation/features/home/pages/home_page.dart';
+import 'package:todo_win/presentation/shared/widgets/alert/alert_collection.dart';
+import 'package:todo_win/presentation/shared/widgets/loading/loading.dart';
 
 void main() {
   group('Home Page Test', () {
@@ -14,5 +16,39 @@ void main() {
       );
       sl.reset();
     });
+
+    testWidgets('find text from home page', (WidgetTester tester) async {
+      setupDependencies();
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: HomePage(),
+        ),
+      );
+      expect(find.text('Home'), findsOneWidget);
+      expect(find.text('Nova Lista'), findsOneWidget);
+      expect(find.byType(IconButton), findsOneWidget);
+      expect(find.byIcon(Icons.info_outline), findsOneWidget);
+      expect(
+          find.widgetWithIcon(IconButton, Icons.info_outline), findsOneWidget);
+      expect(find.byType(Loading), findsOneWidget);
+      sl.reset();
+    });
+
+    testWidgets('click button new list', (WidgetTester tester) async {
+      setupDependencies();
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: HomePage(),
+        ),
+      );
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.byType(AlertCollection), findsOneWidget);
+      expect(find.widgetWithText(AlertCollection, 'Registrar data'),
+          findsOneWidget);
+    });
+    sl.reset();
   });
 }
